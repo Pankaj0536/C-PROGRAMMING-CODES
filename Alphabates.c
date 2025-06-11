338454e09eaa125961d08bf928a9261451740ca2
PANKAJ RANA <pankajrana2754@gmail.com>
2025-06-11 14:48:21 +0530

    Add files via upload

---
+++ Alphabates.c
@@ -0,0 +1,11 @@
+//basic code to display alphabates using for loop
+#include <stdio.h>
+int main()
+{
+    for (char c= 'A'; c <='Z'; c++)
+    {
+       printf("%c\n",c);
+    }
+    
+    return 0;
+}
---
+++ Asciivalue.c
@@ -0,0 +1,13 @@
+// program to Display Ascii values
+#include <stdio.h>
+int main()
+
+{
+    
+   printf("This is ASCCI value table :\n");
+   for (int i = 0; i < 100; i++)
+   {
+   printf("%d -> %c |",i,i);
+   }
+   
+}
