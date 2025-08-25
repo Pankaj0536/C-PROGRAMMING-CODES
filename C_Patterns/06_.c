cf431dfc9c056184f819cff5d20ffa0db876935d
Pankaj0536 <pankajrana2754@gmail.com>
2025-08-25 22:17:39 +0530

    new commit

--- C_Patterns/06_.c
+++ C_Patterns/06_.c
@@ -1,11 +1,39 @@
-/* 
+/*
 
                 *
             *   *   *
         *   *   *   *   *
     *   *   *   *   *   *   *
-   
-   
 
 
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+    printf("Enter the number of rows:\n");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = rows - i; j >= 1; j--)
+        {
+            printf("   ");
+        }
+
+        for (int k = 1; k <= i; k++)
+        {
+            printf(" * ");
+        }
+
+        for (int l = 1; l < i; l++)
+        {
+            printf(" * ");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
