c583e38b4cfa4dc63dc50cfad6f9677c0113d795
Pankaj0536 <pankajrana2754@gmail.com>
2025-08-27 15:40:56 +0530

    new commit

---
+++ C_Patterns/08_.c
@@ -0,0 +1,23 @@
+/* 
+    A 
+    B B
+    C C C
+    D D D D
+
+*/
+
+#include <stdio.h>
+ 
+int main()
+{
+for (char i = 'A'; i < 'E'; i++)
+{
+   for (char j = 'A'; j <= i; j++)
+   {
+    printf(" %c ",i);
+   }
+   printf("\n");
+}
+
+    return 0;
+}
