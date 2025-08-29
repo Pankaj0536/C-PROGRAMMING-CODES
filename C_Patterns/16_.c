2e64bd9105d95a4ab2be5134afdcbbeae4650aa2
Pankaj0536 <pankajrana2754@gmail.com>
2025-08-29 17:45:44 +0530

    New commit

---
+++ C_Patterns/16_.c
@@ -0,0 +1,30 @@
+/* 
+
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+
+*/
+
+#include <stdio.h>
+ 
+int main()
+{
+    int rows , count = 1;
+
+    printf("Enter the number of rows : ");
+    scanf("%d",&rows);
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for ( int j = 1; j <= i; j++)
+        {
+            printf(" %d ",count++);
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
