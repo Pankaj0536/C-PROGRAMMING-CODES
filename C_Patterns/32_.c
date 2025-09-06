c6deeb784daba486aaa63ca03869e02dc38f4506
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-07 00:43:44 +0530

    new commit

--- C_Patterns/32_.c
+++ C_Patterns/32_.c
@@ -1,10 +1,39 @@
-/* 
+/*
 Write a program to print this pattern
- 
+
 1
 0 1
 1 0 1
 0 1 0 1
 
 */
+#include <stdio.h>
 
+int main()
+{
+    int rows, count = 1;
+
+    printf("Enter number of rows : ");
+    scanf(" %d", &rows);
+
+    for (int i = 1; i <= rows; i++)
+    {
+        int p = 1;
+        if (i % 2 == 0)
+            p = 1;
+        else
+            p = 0;
+        for (int j = 1; j <= i; j++)
+        {
+
+            printf(" %d ", p);
+            if (p == 0)
+                p = 1;
+            else
+                p = 0;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
