7ef365db97e2360836addcf129ca399eeb604422
Pankaj0536 <pankajrana2754@gmail.com>
2025-09-06 21:12:34 +0530

    New pattern

--- C_Patterns/18_.c
+++ C_Patterns/18_.c
@@ -1,7 +1,34 @@
-/* 
-1
-0 1
-1 0 1
-0 1 0 1
+/*
+ 0
+ 0  1
+ 0  0  0
+ 0  1  0  1
+
 
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int rows, p, count = 1;
+
+    printf("Enter number of rows : ");
+    scanf(" %d", &rows);
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (count % 2 == 0)
+                p = 1;
+            else
+                p = 0;
+                count ++;
+                printf(" %d ",p);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ C_Patterns/32_.c
@@ -0,0 +1,10 @@
+/* 
+Write a program to print this pattern
+ 
+1
+0 1
+1 0 1
+0 1 0 1
+
+*/
+
